68dc58ceb461a3fb40edae9763c10d8736032c35
mojpm <itsjoelpm@gmail.com>
2021-01-18 16:23:45 +0300

    tiny tiny UI update lol

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -49,11 +49,11 @@ const Quote = () => {
   const Features = () => (
     <div className="feature-container ">
       <div className="modes" onClick={handleClick}>
-        <BsIcons.BsSun className="darkmode" size="30" />
+        <BsIcons.BsSun className="darkmode" size="26" />
       </div>
       <div className="share-twitter">
         <TwitterShareButton url="twitter.com/intent/tweet" title={randomQuote}>
-          <SiIcons.SiTwitter className="twitter-icon" size="32" />
+          <SiIcons.SiTwitter className="twitter-icon" size="26" />
         </TwitterShareButton>
       </div>
       <div className="next-quote" onClick={nextQuote}>
