dfac4f1464b8cb71dc4f6dcdc2ff1a358fd4811d
mojpm <itsjoelpm@gmail.com>
2021-01-28 20:56:23 +0300

    implement Context

--- shoppily/src/App.jsx
+++ shoppily/src/App.jsx
@@ -1,27 +1,14 @@
-import React, { useReducer, useEffect } from "react";
+import React from "react";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
-import Footer from "./Footer";
-import Header from "./Header";
-import Products from "./Products";
-import Detail from "./Detail";
-import Cart from "./Cart";
-import Checkout from "./Checkout";
-import cartReducer from "./cartReducer";
-
-let initialCart;
-try {
-  initialCart = JSON.parse(localStorage.getItem("cart")) ?? [];
-} catch {
-  console.error("Error Could not be parsed to JSON");
-  initialCart = [];
-}
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Products from "./components/Products";
+import Detail from "./components/Detail";
+import Cart from "./components/Cart";
+import Checkout from "./components/Checkout";
 
 export default function App() {
-  const [cart, dispatch] = useReducer(cartReducer, initialCart);
-
-  useEffect(() => localStorage.setItem("cart", JSON.stringify(cart)), [cart]);
-
   return (
     <>
       <div className="content">
@@ -30,20 +17,11 @@ export default function App() {
           <Routes>
             <Route path="/" element={<h1>Welcome to Carved Rock Fitness</h1>} />
             <Route path="/:category" element={<Products />} />
-            <Route
-              path="/:category/:id"
-              element={<Detail dispatch={dispatch} />}
-            />
-            <Route
-              path="/cart"
-              element={<Cart cart={cart} dispatch={dispatch} />}
-            />
-            <Route
-              path="/checkout"
-              element={<Checkout cart={cart} dispatch={dispatch} />}
-            />
+            <Route path="/:category/:id" element={<Detail />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/checkout" element={<Checkout />} />
           </Routes>
-        </main> 
+        </main>
       </div>
       <Footer />
     </>
--- shoppily/src/index.js
+++ shoppily/src/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import ErrorBoundary from './ErrorBoundary'
+import ErrorBoundary from './components/ErrorBoundary'
 import { BrowserRouter as Router } from 'react-router-dom'
+import { CartProvider } from './cartContext'
 
 ReactDOM.render(
     <ErrorBoundary>
         <Router>
-            <App />
+            <CartProvider>
+                <App />
+            </CartProvider>
         </Router>
     </ErrorBoundary>
     , document.getElementById("root")
